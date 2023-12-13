6a97b4260ade4f7dbad9ac5d6b75cd1382d5940b
yaxcat <129409299+yaxcat@users.noreply.github.com>
2023-12-13 10:08:47 -0500

    Updated quick start file

--- Julia/QuickStart.jl
+++ Julia/QuickStart.jl
@@ -43,3 +43,102 @@ println(convert)
 # Can also concatenate like:
 println(str_1*str_2)
 println("$str_1$str_2")
+
+
+# Dictionaries
+# To define a dictionary:
+phonebook = Dict(
+    "Jenny" => "867-5309",
+    "Ghostbusters" => "555-2368"
+    )
+
+println("")
+println("")
+
+# To add an element:
+phonebook["Kramer"] = "555-FILK"
+phonebook
+
+println("")
+println("")
+
+# To delete and return an element
+myitem = pop!(phonebook, "Kramer")
+println(myitem)
+phonebook
+
+# Tuples
+# To create a tuple:
+my_tuple = ("a", "b", "c")
+# Indexing starts at 1 in Julia
+my_tuple[1]
+
+# Arrays
+myarray = [1,2,3,4,5,6,7, "a"]
+# use push!() and pop!() functions to add and remove elements
+push!(myarray, "ZZZ")
+myarray
+pop!(myarray)
+myarray
+
+
+# Loops
+
+# While Loop
+function countup(i)
+    while i < 10
+        i += 1
+        println(i)
+    end
+end
+countup(0)
+
+# For Loop
+for element in myarray
+    println(element)
+end
+
+for n = 1:10
+    println(n)
+end
+
+
+# Conditionals
+
+x = 0
+y = 0
+
+if x > y
+    println("$x is larger than $y")
+elseif y > x
+    println("$y is larger than $x")
+else
+    println("$x and $y are equal")
+end
+
+# This statement
+if x > y
+    x
+else
+    y
+end
+
+# Could be rewritten as:
+(x > y) ? x : y
+
+# functions
+# use the '!' to notate that a function returns a modified/mutated version of the input
+# functions can be defined like:
+function sayhi(name)
+    println("Hi $name, its great to see you!")
+end
+
+sayhi("Mike")
+
+# can also define like this:
+sayhi2(name) = println("Hi $name, its great to see you!")
+
+# can also declare an anonymous function:
+myvar = name -> println("Hi $name, its great to see you!")
+myvar("Chewbacca")
+
