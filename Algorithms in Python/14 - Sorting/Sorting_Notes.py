1f4a154b5765b02a6c971a40a95d3ba604813c5e
yaxcat <129409299+yaxcat@users.noreply.github.com>
2024-02-14 13:28:25 +0000

    Added selection sort

--- Algorithms in Python/14 - Sorting/Sorting_Notes.py
+++ Algorithms in Python/14 - Sorting/Sorting_Notes.py
@@ -20,10 +20,11 @@
 
 
 # Bubble Sort
-# Also sometimes referred to as Sinking Sort
-# Loops over the list repeatedly and compares each pair of adjacent items and then swap them if they are in the wrong order
+# ---------------------------------------------------------------------
 # TC: O(n^2) due to the nested loops
-# SC: O(1)
+# SC: O(1) since sorting is in-place
+# Also sometimes referred to as Sinking Sort
+# Loops over the list repeatedly and compares each pair of adjacent items and then swaps them if they are in the wrong order
 # Each full iteration moves the largest value in the list from wherever it is to the furthest right position it can occupy
 # Therefore, we have one loop tha iterates over the length of the list and another that iterates over the length of the
 # list that has not yet been fully sorted.  This allows us to perform the necessary number of iterations but also avoid
@@ -35,6 +36,32 @@ def bubbleSort(sort_list):
                 sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j] # Swap them
     return sort_list
 
+
+
+# Selection Sort
+# ---------------------------------------------------------------------
+# TC: O(n^2) due to the nested loops
+# SC: O(1) since sorting is in-place
+# Strategy is to repeatedly find the minimum element and then move it to the sorted part of the array which graducally makes the
+# unsorted part sorted.  The algorithm works iteratively and swaps the minimum value found in any given iteration with the first 
+# value in the unsorted part of the array.  The sorted part of the array increases by 1 with each iteration, while the unsorted
+# part decreases by the same amount.  Similar mechanics and drawbacks to Bubble Sort.
+def selectionSort(sort_list):
+    for i in range(0, len(sort_list)): # Defines the length of the entire list
+        min_index = i
+        for j in range(i+1, len(sort_list)): # Defines the section of the list that is unsorted
+            if sort_list[min_index] > sort_list[j]: # If the value at the min index is actually greater than some value we've found in the unsorted section...
+                min_index = j # Change that index to the one where we found that minimum value
+        sort_list[i], sort_list[min_index] = sort_list[min_index], sort_list[i] # Swap those values 
+    return sort_list
+
+
+
+
+# Testing
 my_list = [2,1,7,6,5,3,4,9,8]
 print("\n\n Bubble Sort:")
+print(bubbleSort(my_list))
+
+print("\n\n Selection Sort:")
 print(bubbleSort(my_list))
