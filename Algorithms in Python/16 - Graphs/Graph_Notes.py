f26bfafb16635a1b669704eb0b0a1cce7f384288
yaxcat <129409299+yaxcat@users.noreply.github.com>
2024-02-29 13:35:15 +0000

    Added graph notes

---
+++ Algorithms in Python/16 - Graphs/Graph_Notes.py
@@ -0,0 +1,29 @@
+# GRAPH TERMINOLOGY
+#   1) Vertices - nodes of the graph
+#   2) Edge - line that connects pairs of vertices
+#   3) Unweighted graph - a graph in which all edges have the same value
+#   4) Weighted graph - a graph in which the values associated with edges differ
+#   5) Undirected graph - a graph in which the edges do not have a direction associated with them
+#   6) Directed graph - edges have a direction associated with them
+#   7) Cyclic graph - a graph which contains at least one loop
+#       - A collection of nodes and edges arranged such that they can be transited completely while traversing back to
+#       - to the starting point
+#   8) Acyclic graph - graph with no loops
+#   9) Tree - a special case of a directed and acyclic graph
+
+
+
+# GRAPH TYPES
+
+#   1) Unweighted Undirected 
+#   2) Unweighted Directed - Can be bidirectional or unidirectional
+#   3) Positive Weighted Undirected - weights are positive numbers
+#   4) Positive Weighted Directed
+#   5) Negative Weighted Undirected - weights can be both positive and negative numbers
+#   6) Negative Weighted Directed
+
+# GRAPH REPRESENTATION
+
+#   1) Adjacency Matrix - 2D array equal to the size of the nuumber of nodes **2.  Elements in the matrix indicate whether the pairs
+#      of vertices share an edge or not (are adjacent).  See graphic
+
