2a4a9db326303e1ae2c07adee7b57b38e7a62291
yaxcat <129409299+yaxcat@users.noreply.github.com>
2024-03-04 13:48:34 +0000

    Finished graph class

--- Algorithms in Python/16 - Graphs/Graph_Notes.py
+++ Algorithms in Python/16 - Graphs/Graph_Notes.py
@@ -49,15 +49,60 @@ class Graph:
             self.adj_list[vertex] = []
             return "Node added to graph"
 
+    # Adds an edge between two vertices if they both exist in the graph    
+    def addEdge(self, vertex1, vertex2):
+        # Check to make sure the vertices are in the graph
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            # Establish the edge by adding vertices to the appropriate places in the adjacency list
+            self.adj_list[vertex1].append(vertex2)
+            self.adj_list[vertex2].append(vertex1)
+            return f"Vertex added between {vertex1} and {vertex2}"
+        else:
+            return "Could not add edge since one or more nodes is missing from graph"
+        
+    # Removes an edge from the graph
+    def removeEdge(self, vertex1, vertex2):
+        if vertex1 in self.adj_list.keys() and vertex2 in self.adj_list.keys():
+            # Try to call the list's remove method on the appropriate vertex.  Both nodes may be in the graph but not actually have an
+            # edge.  This would throw an error so we use try/accept.
+            try:
+                self.adj_list[vertex1].remove(vertex2)
+                self.adj_list[vertex2].remove(vertex1)
+                return f"Edge between {vertex1} and {vertex2} successfully removed"
+            except ValueError: # If the node is present in the graph but not the particular section of the adj_list
+                return f"{vertex1} and {vertex2} present, but there is not a symetrical edge between them to remove"
+        else:
+            return "Failed to remove edge, one or nodes is missing"
+        
+    # Remove a vertex from the graph
+    # The strategy is to identify the vertex that needs to go and use its list of edges to remove the vertex we want to get rid of from
+    # its neighbor nodes' lists.  That way, we don't have a bunch of dead/non-functional edges left over.  After this step, we delete the
+    # node itself.
+    def removeVertex(self, vertex_to_delete):
+        if vertex_to_delete in self.adj_list.keys():
+            # Find the vertex we wish to delete and loop through its list of neighbors/edges
+            for neighbor_vertex in self.adj_list[vertex_to_delete]:
+                self.adj_list[neighbor_vertex].remove(vertex_to_delete) # For each neighbor/edge delete the target vertex from the neighbor vertex's edge list
+            del self.adj_list[vertex_to_delete] # Finally delete the target vertex itself
+            return f"The target vertex {vertex_to_delete} has been deleted and its edges removed."
+        return f"Unable to delete the target vertex {vertex_to_delete}"
+
     def printGraph(self):
         for vertex in self.adj_list:
             print(vertex, ":", self.adj_list[vertex])
 
+    
+
 
 my_graph = Graph()
 print(my_graph.addVertex('a'))
+print(my_graph.addVertex('b'))
 print(my_graph.printGraph())
-print(my_graph.addVertex('a'))
+print(my_graph.addEdge('a', 'b'))
+print(my_graph.printGraph())
+print(my_graph.removeEdge('a', 'b'))
+print(my_graph.printGraph())
+
 
 
 my_graph_old = {
@@ -76,3 +121,9 @@ my_graph = {
     'd': ['a','c','e'],
     'e': ['b','d']
 }
+print("\n\n\n\n\n")
+my_new_graph = Graph(my_graph)
+my_new_graph.printGraph()
+print("\n\n")
+my_new_graph.removeVertex('e')
+my_new_graph.printGraph()
