946312ce0ce426e62f2326ee810abe8bcf00cb72
yaxcat <129409299+yaxcat@users.noreply.github.com>
2024-03-19 12:09:50 +0000

    Finished Dijikstra's implementation

--- Algorithms in Python/19 - Dijkstras Algorithm/Dijikstra Implementation.py
+++ Algorithms in Python/19 - Dijkstras Algorithm/Dijikstra Implementation.py
@@ -1,5 +1,13 @@
 # Implementation is much less messy if we just use a few classes
 
+# Dijikstra's algorithm does not work with negative numbers
+# Negative cycle:
+#   - Cycle - a path which allows you to start from and return to the same vertex (a loop)
+#   - Negative cycle weight - the total weight of the path is negative
+# Dijikstra will keep hitting this path over and over because the cost keeps getting lower each
+# time, will run to inifinity
+
+
 import heapq # Just use the standard Python library to make things easier
 
 class Edge:
@@ -42,13 +50,13 @@ class Dijikstra:
                 continue # Skips to the next unvisited node if we encountered a visited one
             # Extract the lowest cost node from the heap, since this is a minimum heap, we need to pop the 
             # first element.  This will be the starting node at first
-            current_vertex = heapq.heappop(self.heap) # Extract the lowest cost node from the heap, since this is a minimum heap, we need to pop the first element.  This will be the starting node at first
-            # Now, for the vertex we're at, examine its neighbors to understand their cost/distance from the current node
+            current_vertex = heapq.heappop(self.heap) 
+            # Now, for the minimum cost node we've chosen, explore its edges
             for edge in current_vertex.neighbors:
-                edge_start_vertex = edge.start_vertex # The vertex our current node connects to via this edge
-                edge_end_vertex = edge.end_vertex # The next vertex after that
-                path_distance = edge_start_vertex.min_distance + edge.weight # The distance from the current vertex to the end of this edge (a three node span)
-                # If the path distance is less than the currently calculated distance to that end node, we must update it
+                edge_start_vertex = edge.start_vertex # Current minimum node
+                edge_end_vertex = edge.end_vertex # Node at which this edge terminates
+                path_distance = edge_start_vertex.min_distance + edge.weight # The minimum path distance so far plus the distance to the termination node
+                # If the path distance we just computed is less than the previously calculated distance to that end node, we must update it
                 if path_distance < edge_end_vertex.min_distance:
                     edge_end_vertex.min_distance = path_distance
                     edge_end_vertex.predecessor = edge_start_vertex # Must update the predecessor too, since its quicker to get there from that node
@@ -57,4 +65,15 @@ class Dijikstra:
                     # they are 'inert' for our purposes.  We live with dupes rather update the heap object
                     # itself because updating would take O(N) time, and we want this process to be efficient
                     heapq.heappush(self.heap, edge_end_vertex) 
-            current_vertex.visited = True
+            current_vertex.visited = True # We've now fully explored the toplogy of the current node, so mark it as visited.
+
+
+    # Prints the shortest path
+    def get_shortest_path(self, vertex):
+        print("Total Path Length: ", vertex.min_distance)
+        current_vertex = vertex
+        while current_vertex is not None: # Until we get to the beginning of the path
+            print(current_vertex.name, end="-") # Print the current vertex
+            current_vertex = current_vertex.predecessor # And set the current vertex to be that of its predecessor
+
+
